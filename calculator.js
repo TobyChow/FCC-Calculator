570b6276bf7e4eeb4fa42c7ab50b094759168ede
Toby Chow <toby_chow@hotmail.com>
2016-04-03 18:10:52 +0800

    complete numerical and AC operations

--- calculator.js
+++ calculator.js
@@ -3,10 +3,13 @@ $(document).ready(function() {
     //Remove initial 0 if a number greater than 0 is inputted
     var count = 0;
     $(".num").click(function() {
+        //Display input if first input is not 0
         if (($(this).text()) > 0 && count < 1) {
             $("#bot-display").empty();
-            var number = $(this).text();
             $("#bot-display").append($(this).text());
+            //update sum 
+            var number = Number($(this).text());
+            sum += number;
             count++;
             //Prevents multiple input of 0s if zero is the first and only value
         } else if (($(this).text()) === '0' && count < 1) {
