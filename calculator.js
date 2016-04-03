65a4c929cfe3b61363fa0c429b96939147efdc72
Toby Chow <toby_chow@hotmail.com>
2016-04-03 20:05:31 +0800

    complete add funtion

--- calculator.js
+++ calculator.js
@@ -3,13 +3,10 @@ $(document).ready(function() {
     //Remove initial 0 if a number greater than 0 is inputted
     var count = 0;
     $(".num").click(function() {
-        //Display input if first input is not 0
+        //Condition for first input and is a not 0
         if (($(this).text()) > 0 && count < 1) {
             $("#bot-display").empty();
             $("#bot-display").append($(this).text());
-            //update sum 
-            var number = Number($(this).text());
-            sum += number;
             count++;
             //Prevents multiple input of 0s if zero is the first and only value
         } else if (($(this).text()) === '0' && count < 1) {
@@ -19,17 +16,30 @@ $(document).ready(function() {
         }
     });
 
-    //All Clear function
+    //All Clear function - reset sum, count, and display
     $("#AC").click(function() {
         $("#bot-display").empty();
         $("#top-display").empty();
         $("#bot-display").append('0');
+        sum = 0;
         count = 0;
     });
 
     //Equal function
     $("#equal").click(function() {
+        var current = Number($("#bot-display").text());
+        sum += current;
         $("#bot-display").empty();
         $("#bot-display").append(sum);
+    });
+
+    //Operator function
+    $(".op").click(function() {
+        if (this.id == 'plus') {
+            var current = Number($("#bot-display").text());
+            sum += current;
+            $("#top-display").append(current + ' ' + '+');
+            count = 0;
+        }
     });
 });
